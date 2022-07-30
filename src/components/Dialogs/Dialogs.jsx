1ac28dda3cf78e94b1a0e286a7e71ad9558c5c51
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2022-07-30 05:46:13 +0300

    21. messages page layouting

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import classes from './Dialogs.module.css';
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    Gektor
+                </div>
+                <div className={classes.dialog}>
+                    Kesha
+                </div>
+                <div className={classes.dialog}>
+                    Jeka
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi! All good?</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>What are you doing?</div>
+            </div>
         </div>
     );
 }
